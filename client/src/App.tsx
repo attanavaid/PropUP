1352c663c73bb5f0653610cc2020ac78244f7a7e
Atta Navaid <attanavaid@gmail.com>
2023-02-24 16:48:08 -0500

    Changed server URL

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -59,7 +59,7 @@ function App() {
 
       // Save user to MongoDB...
       if (profileObj) {
-        const response = await fetch("http://localhost:8080/api/v1/users", {
+        const response = await fetch("https://propup.onrender.com/api/v1/users", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
@@ -131,7 +131,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://propup.onrender.com/api/v1")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
